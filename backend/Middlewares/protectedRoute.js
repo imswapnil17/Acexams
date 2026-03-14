6f912e523689d85b9454a89adc6fb2cda8591e0a
imswapnil17 <imswapnil17@gmail.com>
2026-03-14 13:54:45 +0530

    Protected Route Added

---
+++ backend/Middlewares/protectedRoute.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+import { ENV_VARS } from "../config/config.js"
+import User from "../Models/UserModel.js"
+export const protectedRoute = async (req,res,next)=>{
+    try{
+        if(!req.cookies){
+        return res.status(400).json({success:false,message:"Please login to continue"})
+    }
+    if(!req.cookies['jwt-acexams']){
+        return res.status(400).json({success:false,message:"Please login to continue"})
+    }
+    const userId = jwt.verify(req.cookies['jwt-acexams'],ENV_VARS.JWT_TOKEN).userId;
+    const findUser = await User.findOne({_id:userId},("name username email -_id"))
+    if(!findUser){
+        return res.status(400).json({success:false,message:"Please login to continue"})
+    }
+    next()
+    
+    }
+    catch (error){
+        console.error(error)
+        return res.status(500).json({success:false,message:"Internal Server Error"})
+    }
+}
